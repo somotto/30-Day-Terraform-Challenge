8f6071d88923312ab5e61dfae6ad72a05891655a
somotto <stephenonyango4783@gmail.com>
2026-03-23 11:25:07 +0300

    add variables

---
+++ Day5/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for web servers"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "server_port" {
+  description = "Port the web server listens on (instances)"
+  type        = number
+  default     = 8080
+}
+
+variable "desired_capacity" {
+  description = "Desired number of instances in the ASG"
+  type        = number
+  default     = 2
+}
+
+variable "min_size" {
+  description = "Minimum number of instances in the ASG"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in the ASG"
+  type        = number
+  default     = 5
+}
